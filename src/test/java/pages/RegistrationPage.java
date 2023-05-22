bef640c7d19176b4ce96b3e9651f67ba587c25e7
Michal Dobrzycki <dobrzycki@gmail.com>
2023-05-22 16:43:15 +0200

    PageFactory

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -3,25 +3,30 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
-public class RegistrationPage {
-    private WebDriver driver;
+public class RegistrationPage extends BasePage {
+
+    @FindBy(css="input[type='text']")
+    WebElement usernameInput;
+
+    @FindBy(css="input[type='email']")
+    WebElement mailInput;
+
+    @FindBy(css="input[type='password']")
+    WebElement passwordInput;
+
+    @FindBy(css = "button.btn-primary")
+    WebElement button;
 
     public RegistrationPage(WebDriver driver) {
-        this.driver = driver;
+        super(driver);
     }
 
     public void registerAs(String username, String mail, String password) {
-        WebElement usernameInput = driver.findElement(By.cssSelector("input[type='text']"));
         usernameInput.sendKeys(username);
-
-        WebElement mailInput = driver.findElement(By.cssSelector("input[type='email']"));
         mailInput.sendKeys(mail);
-
-        WebElement passwordInput = driver.findElement(By.cssSelector("input[type='password']"));
         passwordInput.sendKeys(password);
-
-        WebElement button = driver.findElement(By.cssSelector("button.btn-primary"));
         button.click();
     }
 
